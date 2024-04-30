154cc92220508bd74bd89686e9ea9aa7a8fb39be
Juan Manuel Del Castillo Navarro <delcastillonavarro@outlook.es>
2024-04-30 15:01:44 +0200

    Creación AccommodationUnitServiceController

---
+++ aloha/src/main/java/com/example/aloha/repositories/AccommodationUnitServiceRepository.java
@@ -0,0 +1,9 @@
+package com.example.aloha.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.aloha.models.AccommodationUnitService;
+
+public interface AccommodationUnitServiceRepository extends JpaRepository<AccommodationUnitService, Long> {
+
+}
