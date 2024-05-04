f3ec955c6b6f3c0fbb64944f14aa972c68baada1
Juan Manuel Del Castillo Navarro <delcastillonavarro@outlook.es>
2024-05-05 00:32:11 +0200

    Finalización implementación JWT (No se permite acceso al controlador)

--- aloha/src/main/java/com/example/aloha/config/JwtService.java
+++ aloha/src/main/java/com/example/aloha/config/JwtService.java
@@ -2,6 +2,8 @@ package com.example.aloha.config;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,13 +11,29 @@ import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "5F534E47A47A9ED1A89127871B4F8";
+    private static final String SECRET_KEY = "5c3199f7ade13ed95b88076299ab70412b70c456b57cfc57225139ec0ac7ac40";
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
     public String getUserName(String token) {
         return getClaim(token, Claims::getSubject);
--- aloha/src/main/java/com/example/aloha/models/User.java
+++ aloha/src/main/java/com/example/aloha/models/User.java
@@ -15,10 +15,18 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User implements UserDetails {
 
     @Id
