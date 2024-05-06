4b08b7d8b3c608f2d6dcef8fe8d37ef08260f947
Del Castillo Navarro <jmcastillon@minsait.com>
2024-05-06 12:56:17 +0200

    Eliminación JWT

--- aloha/src/main/java/com/example/aloha/repositories/ClientCardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.aloha.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.aloha.models.ClientCard;
-
-public interface ClientCardRepository extends JpaRepository<ClientCard, Long> {
-
-}
