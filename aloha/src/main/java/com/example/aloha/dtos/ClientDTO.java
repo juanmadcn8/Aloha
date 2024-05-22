b08190f32329987346d76daeb2525451371bf3bf
Del Castillo Navarro <jmcastillon@minsait.com>
2024-05-22 15:36:41 +0200

    Fixed ClientDTO

--- aloha/src/main/java/com/example/aloha/dtos/ClientDTO.java
+++ aloha/src/main/java/com/example/aloha/dtos/ClientDTO.java
@@ -1,9 +1,13 @@
 package com.example.aloha.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class ClientDTO {
     private String email;
